2f9fc535195f9f2747083442f43999fe2c44fcf7
Neto <ukpongneto@wellahealth.com>
2023-03-25 05:25:00 +0100

    database models completed

--- app/models/database_models.py
+++ app/models/database_models.py
@@ -8,9 +8,10 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    phoneNumber = Column(String)
+    phoneNumber = Column(String, nullable=False)
     createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
 
@@ -19,11 +20,12 @@ class PropertyRequest(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     location = Column(String, nullable=False)
-    property = Column(String, nullable=False)
-    bedroom = Column(Integer)
+    propertyType = Column(String, nullable=False)
+    numberOfrooms = Column(Integer)
     budgetRange = Column(String, nullable=False)
     maxBudget = Column(String, nullable=False)
     notes = Column(String)
+    isPaid = Column(Boolean, nullable=False)
     userId = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
@@ -34,5 +36,101 @@ class HotelRequest(Base):
     __tablename__ = "hotelRequests"
 
     id = Column(Integer, primary_key=True, nullable=False)
+    checkInDate = Column(String, nullable=False)
+    checkOutDate = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    numberOfGuests = Column(Integer, nullable=False)
+    numberOfRooms = Column(Integer, nullable=False)
+    budgetPerRoom = Column(Integer, nullable=False)
+    isPaid = Column(Boolean, nullable=False)
     createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    
+
+
+class MeetingRoomRequest(Base):
+    __tablename__ = "meetingRoomRequests"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    checkInDate = Column(String, nullable=False)
+    checkOutDate = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    numberOfGuests = Column(Integer, nullable=False)
+    location = Column(String, nullable=False)
+    budgetPerDay = Column(Integer, nullable=False)
+    numberOfDays = Column(Integer, nullable=False)
+    isPaid = Column(Boolean, nullable=False)
+    createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+class EventHallRequest(Base):
+    __tablename__ = "eventHallRequests"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    numberOfGuests = Column(Integer, nullable=False)
+    budgetPerDay = Column(Integer, nullable=False)
+    numberOfDays = Column(Integer, nullable=False)
+    isPaid = Column(Boolean, nullable=False)
+    createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+class OfficeSpaceRequest(Base):
+    __tablename__ = "officeSpaceRequests"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    checkInDate = Column(String, nullable=False)
+    checkOutDate = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    numberOfWorkers = Column(Integer, nullable=False)
+    numberOfWorkstations = Column(Integer, nullable=False)
+    budgetPerDay = Column(Integer, nullable=False)
+    numberOfDays = Column(Integer, nullable=False)
+    isPaid = Column(Boolean, nullable=False)
+    createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+class PropertyManagement(Base):
+    __tablename__ = "propertyManagementRequests"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+class ShortletRequest(Base):
+    __tablename__ = "shortletRequests"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    checkInDate = Column(String, nullable=False)
+    checkOutDate = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    numberOfRooms = Column(Integer, nullable=False)
+    budgetPerDay = Column(Integer, nullable=False)
+    numberOfDays = Column(Integer, nullable=False)
+    isPaid = Column(Boolean, nullable=False)
+    createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+class Consultation(Base):
+    __tablename__ = "consultations"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    location = Column(String, nullable=False)
+    phoneNumber = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    message = Column(String, nullable=False)
+    contactMethod = Column(String, nullable=False)
+    createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
