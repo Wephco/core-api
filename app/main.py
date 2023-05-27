ed82e161df20a05692ded736f1e162f2f33ecf7d
Neto <ukpongneto@wellahealth.com>
2023-05-27 19:07:08 +0100

    main function added

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 # from .db import database_models, database
 from .routers import users, auth, property_requests, hotel_requests, agents
@@ -25,3 +26,7 @@ app.include_router(auth.router)
 app.include_router(property_requests.router)
 app.include_router(hotel_requests.router)
 app.include_router(agents.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
