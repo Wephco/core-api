4b829cc05be8cd2f9ffd4cf6ac58d44faaa62b9b
Neto <ukpongneto@wellahealth.com>
2023-05-31 14:39:53 +0100

    postgresql connection string for alembic modified

--- alembic/env.py
+++ alembic/env.py
@@ -11,7 +11,7 @@ from app.environment_config import settings
 # access to the values within the .ini file in use.
 config = context.config
 config.set_main_option(
-    "sqlalchemy.url", f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}?sslmode=verify-full')
+    "sqlalchemy.url", f'cockroachdb://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
