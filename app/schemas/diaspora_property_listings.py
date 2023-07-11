933d946d1749c3cc04ffba748fa2435a4fa81615
Neto <ukpongneto@wellahealth.com>
2023-07-11 12:48:31 +0100

    diaspora property listing schema updated

--- app/schemas/diaspora_property_listings.py
+++ app/schemas/diaspora_property_listings.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 from datetime import datetime
 
 
@@ -13,7 +13,7 @@ class DiasporaPropertyListingBase(BaseModel):
     numberOfLivingRooms: int
     numberOfKitchens: int
     agentId: int
-    agentName: str
+    agentName: Optional[str] = ''
 
     class Config:
         orm_mode = True
