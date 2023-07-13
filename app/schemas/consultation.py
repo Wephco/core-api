ce7f2b65cf9bce6e41f6d3c8ce7efb27d68b05a0
Neto <ukpongneto@wellahealth.com>
2023-07-13 18:37:50 +0100

    changes to consultation schema

--- app/schemas/consultation.py
+++ app/schemas/consultation.py
@@ -15,9 +15,13 @@ class ConsultationBase(BaseModel):
         orm_mode = True
 
 
-class ConsultationResponse(ConsultationBase):
+class ConsultationResponse(BaseModel):
     id: int
     userId: int
+    user: UserBase
+    service: str
+    message: str
+    contactMethod: str
     createdAt: datetime
 
     class Config:
